8ccba899977c59e3309810a670833d676f71def4
greaseduppig2011 <22110@fraser.school.nz>
2025-03-06 12:38:12 +1300

    The actual code update
    I'm actually doing the coding now isn't that insane

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print('Goacumhuge gaming')
+#Func goes here
+def make_statement(statement, decoration):
+    print(f"{decoration * 3} {statement} {decoration * 3}")
+
+
+# Main code
+make_statement("Goacumhuge", "8")
