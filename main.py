2d275d8eed0fc7418e7f78005d4497bbd1c6119f
greaseduppig2011 <22110@fraser.school.nz>
2025-03-06 13:40:41 +1300

    step 3 attempt
    attempt on step 3.

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 #Func goes here
-def make_statement(statement, decoration):
+def make_statement(statement, decoration, lines):
+    for i in range(int(lines/2) + (lines%2) -1 ): 
+        print(f"{decoration * 3}{decoration * len(statement)}{decoration*3}")
     print(f"{decoration * 3} {statement} {decoration * 3}")
+    for i in range(int(lines/2)):
+        print(f"{decoration * 3}{decoration * len(statement)}{decoration*3}")
+    
 
 
 # Main code
-make_statement("Goacumhuge", "8")
+make_statement("Goacumhuge", "=", 2)
