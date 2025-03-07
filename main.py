23a6adb7eb3c12eecdf16d8128aa2e22152dcd32
greaseduppig2011 <22110@fraser.school.nz>
2025-03-07 14:03:14 +1300

    Small spacing fix
    Fix spacing issues

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 def make_statement(statement, decoration, lines):
     for i in range(int(lines/2) + (lines%2) -1 ): 
         print(f"{decoration * 3}{decoration * len(statement)}{decoration*3}")
-    print(f"{decoration * 3} {statement} {decoration * 3}")
+    print(f"{decoration * 3}{statement}{decoration * 3}")
     for i in range(int(lines/2)):
         print(f"{decoration * 3}{decoration * len(statement)}{decoration*3}")
     
 
 
 # Main code
-make_statement("Goacumhuge", "=", 2)
+make_statement("Goacumhuge", "=", 3)
